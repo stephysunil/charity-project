4f2dbdf4c962a8f33dc68255661f0f5b4a8e307c
stephysunil <stephysunil19@gmail.com>
2024-08-09 15:49:04 +0530

    test

--- appname/urls.py
+++ appname/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,demo
 
 urlpatterns = [
-    path('',index,name='index')
-    path ('test/',demo)
+    path('',index,name='index'),
+    path ('test/',demo),
 ]
