785ffe989d929c1ff83ae290dd4e96532157ea5f
stephysunil <stephysunil19@gmail.com>
2024-08-09 16:06:16 +0530

    1

--- appname/models.py
+++ appname/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class userdetail(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.models.CharField( max_length=50)
+    mobile_no=models.models.BigIntegerField()
