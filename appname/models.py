33a5d26a20638ae6890925db23fb8b9e829eca3e
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-08-09 16:09:06 +0530

    Test

--- appname/models.py
+++ appname/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class userdetail(models.Model):
-    name=models.CharField(max_length=50)
-    address=models.models.CharField( max_length=50)
-    mobile_no=models.models.BigIntegerField()
+class UserDetail(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    mobile_no = models.BigIntegerField()
