bc6d7d7994bd6e7a08c27f0c23fa29df0b08dd8e
stephysunil <stephysunil19@gmail.com>
2024-08-12 23:23:12 +0530

    admin page

--- appname/admin.py
+++ appname/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from appname.models import UserDetail,SponserDetail,SponserShip,MedicalTeam,Feedback
 
 # Register your models here.
+admin.site.register(UserDetail)
+admin.site.register(SponserDetail)
+admin.site.register(SponserShip)
+admin.site.register(MedicalTeam)
+admin.site.register(Feedback)
