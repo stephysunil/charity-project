18ef0650971007d725010df3f7a7a810e426f177
stephysunil <stephysunil19@gmail.com>
2024-08-26 23:59:43 +0530

    templates

--- appname/urls.py
+++ appname/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index,test,login,regspo
+from .views import index,test,login,regspo,regus,spoboard,userboard
 
 urlpatterns = [
     path('',index,name='index'),
     path ('test/',test),
     path ('login/',login),
     path ('regspo/',regspo),
+    path ('regus/',regus),
+    path ('spoboard/',spoboard),
+    path ('userboard/',userboard),
 ]
--- appname/views.py
+++ appname/views.py
@@ -12,3 +12,12 @@ def login(request):
 
 def regspo(request):
     return render(request,'appname/regspo.html')
+
+def regus(request):
+    return render(request,'appname/regus.html')
+
+def spoboard(request):
+    return render(request,'appname/spoboard.html')
+
+def userboard(request):
+    return render(request,'appname/userboard.html')
