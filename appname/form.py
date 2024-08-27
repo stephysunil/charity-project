d552330ca446ff7f3f0d348b893b1897afa8aa97
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-08-27 12:11:25 +0530

    .

--- appname/form.py
+++ appname/form.py
@@ -1,26 +1,26 @@
 from django import forms
-from django.contrib auth.models import User 
-from django.db import auth.models
+from django.contrib.auth.models import User
+from django.db import models
 from .models import UserDetail,SponsorDetail
 
 class userForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput())
     class Meta():
-        models=User
+        model=User
         fields=['first_name','last_name','username','email','password'] 
 
 class userAddForm(forms.ModelForm):
     class Meta():
-        models=UserDetail
-        fields=['umobile_no','uaddress','uloctaion','uphoto','description','no_year']
+        model=UserDetail
+        fields=['umobile_no','uaddress','ulocation' ,'uphoto','description','no_year']
 
-class sponsorForm():
+class sponsorForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput())
     class Meta():
-        models=User
+        model=User
         fields=['first_name','last_name','username','email','password'] 
 
 class sponsorAddForm(forms.ModelForm):
     class Meta():
-        models=SponsorDetail
+        model=SponsorDetail
         fields=['smobile_no']
--- appname/views.py
+++ appname/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .form import sponsorAddForm,sponsorForm
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,22 @@ def login(request):
     return render(request,'appname/login.html')
 
 def regspo(request):
-    return render(request,'appname/regspo.html')
+    registered = False
+    if request.method == 'POST':
+        var_sponsorForm = sponsorForm(request.POST)
+        var_sponsorAddForm = sponsorAddForm(request.POST)
+        if var_sponsorForm.is_valid() and var_sponsorAddForm.is_valid():
+            sponsorprimary = var_sponsorForm.save()
+            sponsorprimary.set_password(sponsorprimary.password)
+            sponsorprimary.save()
+            sponsorAdd = var_sponsorAddForm.save(commit=False)
+            sponsorAdd.sponsor = sponsorprimary
+            sponsorAdd.save()
+            registered = True
+    else:
+        var_sponsorForm = sponsorForm()
+        var_sponsorAddForm = sponsorAddForm()
+    return render(request,'appname/regspo.html', {'var_sponsorForm': var_sponsorForm, 'var_sponsorAddForm': var_sponsorAddForm, 'registered': registered})
 
 def regus(request):
     return render(request,'appname/regus.html')
@@ -21,3 +37,4 @@ def spoboard(request):
 
 def userboard(request):
     return render(request,'appname/userboard.html')
+
